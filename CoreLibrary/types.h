0b298b914e73d42363e51bded22031d2e706d8bb
eric@ru.is <eric@ru.is@ed8dfbe9-f39a-40df-8c8a-baf52719d138>
2010-10-24 21:18:21 +0000

    Compilation: removed the STL flags; removed DebugVisualizer configuration.
    git-svn-id: http://cadia.ru.is/svn/repos/CoreLibrary/trunk@37 ed8dfbe9-f39a-40df-8c8a-baf52719d138

--- CoreLibrary/types.h
+++ CoreLibrary/types.h
@@ -31,13 +31,6 @@
 #ifndef core_types_h
 #define core_types_h
 
-#ifndef	DEBUG_VISUALIZER
-#undef	_SECURE_SCL
-#define _SECURE_SCL 1
-#undef	_HAS_ITERATOR_DEBUGGING
-#define _HAS_ITERATOR_DEBUGGING 0
-#endif
-
 #include	<cstddef>
 
 #if defined(WIN32)
